c6039768ef5565e5e43ab87c153583a61026edb0
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-10-04 15:41:31 +0530

    arrow func and object

---
+++ func1.js
@@ -0,0 +1,13 @@
+const product = (a, b) => {
+    return a * b;
+}
+
+console.log(product(7, 3));
+
+let student = {
+    name: "Raj Sharma",
+    age: 18,
+    school: "St. Teresa School",
+    hobbies: ["dancing", "singing", "painting"]
+};
+console.log(student)
