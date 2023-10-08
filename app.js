9e2663f096f97769ba647d6d407fc0a42ca4f4bd
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-10-08 14:08:48 +0530

    home, app, node page added

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("Yawar Ejaz");
+    res.setHeader("Content-Type", "text/html")
+    let url = req.url
+    if (url == "/home") {
+        res.write("<html>")
+        res.write("<head><title>Response Page</title></head>")
+        res.write("<body><h1>Home Page</h1></body>")
+        res.write("</html>")
+    }
+    else if (url == "/about") {
+        res.write("<html>")
+        res.write("<head><title>Response Page</title></head>")
+        res.write("<body><h1>About Page</h1></body>")
+        res.write("</html>")
+    }
+    else if (url == "/node") {
+        res.write("<html>")
+        res.write("<head><title>Response Page</title></head>")
+        res.write("<body><h1>Welcome to Node Js Project</h1></body>")
+        res.write("</html>")
+    }
+    else{
+        res.write("<html>")
+        res.write("<head><title>Response Page</title></head>")
+        res.write("<body><h1>Blank Page</h1></body>")
+        res.write("</html>")
+    }
+
+    res.end()
 });
 
-server.listen(4000);
+server.listen(3000);
